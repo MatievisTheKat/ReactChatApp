93982d61536637dd404a1765d465d1edb4024dd8
MatievisTheKat <matievisthekat@gmail.com>
2020-07-23 14:54:33 +0200

    general layout done

---
+++ src/components/ChatBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class ChatBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="container"></div>
+      </div>
+    );
+  }
+}
